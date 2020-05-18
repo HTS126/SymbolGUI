7a083e0d96ac37755166727a6facfdc110e5f066
Harry Smith <harrytsmith126@outlook.com>
2020-05-18 20:17:30 +0100

    Add project files.

---
+++ SymbolGUI/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SymbolGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SoT = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SoT
+            // 
+            this.SoT.Tick += new System.EventHandler(this.SoT_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Move Back To Bottom Right",
+            "Close Software",
+            "≥ - \"Greater than or equal to\"",
+            "≤ - \"Less than or equal to\"",
+            "≠ - \"Not equals\"",
+            "± - \"Plus-minus sign\"",
+            "∧ - \"And\"",
+            "∨ - \"Or\"",
+            "¬ - \"Not\"",
+            "≡ - \"Exactly Equal To\"",
+            "⇒ - \"Implies\"",
+            "⇔ - \"If and only if\"",
+            "↓ - \"NOR\"",
+            "↑ - \"NAND\"",
+            "∃ - \"Existential Quantifier\"",
+            "∀ - \"Universal Quantifier\"",
+            "Ø - \"Empty Set\"",
+            "∈ - \"Set Membership\"",
+            "∉ - \"Set Membership Negation\"",
+            "⊆ - \"Subset\"",
+            "⊂ - \"Proper Subset\"",
+            "∪ - \"Union\"",
+            "∩ - \"Intersection\"",
+            "⨄ - \"Disjoint Union\"",
+            "↔ - \"Binary Relation\"",
+            "→ - \"Function\"",
+            "◦ - \"Relation\"",
+            "• - \"Matrix\"",
+            "⌊ - \"Floor/Round Open\"",
+            "⌋ - \"Floor Close\"",
+            "⌈ - \"Ceiling Open\"",
+            "⌉ - \"Ceiling/Round Close\"",
+            "α – \"Alpha Lower\"",
+            "β – \"Beta Lower\"",
+            "Δ - \"Delta Upper\"",
+            "δ – \"Delta Lower\"",
+            "θ – \"Theta Lower\"",
+            "λ – \"Lambda Lower\"",
+            "μ – \"Mu Lower\"",
+            "π - \"Pi Lower\"",
+            "Σ – \"Sigma Upper\"",
+            "σ – \"Sigma Lower\"",
+            "Ω - \"Omega Upper\"",
+            "ω – \"Omega Lower\"",
+            "∞ - \"Infinity\"",
+            "- - \"Combining Overline / Bar\""});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(338, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Copy to Clipboard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 126);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "SymbolGUI 3.5";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer SoT;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SymbolGUI/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using System.Net.Mail;
+using System.Net.Http;
+using Microsoft.VisualBasic;
+
+namespace SymbolGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width, workingArea.Bottom - Size.Height);
+        }
+
+        private void SoT_Tick(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            //Rectangle workingArea = Screen.GetWorkingArea(this);
+            //this.Location = new Point(workingArea.Right - Size.Width, workingArea.Bottom - Size.Height);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Move Back To Bottom Right")
+            {
+                Rectangle workingArea = Screen.GetWorkingArea(this);
+                this.Location = new Point(workingArea.Right - Size.Width, workingArea.Bottom - Size.Height);
+            }
+            else if (comboBox1.Text == "Close Software")
+            {
+                Environment.Exit(0);
+            }
+            else if (comboBox1.Text == "≥ - \"Greater than or equal to\"")
+            {
+                Clipboard.SetText("≥");
+            }
+            else if (comboBox1.Text == "≤ - \"Less than or equal to\"")
+            {
+                Clipboard.SetText("≤");
+            }
+            else if (comboBox1.Text == "≠ - \"Not equals\"")
+            {
+                Clipboard.SetText("≠");
+            }
+            else if (comboBox1.Text == "± - \"Plus - minus sign\"")
+            {
+                Clipboard.SetText("±");
+            }
+            else if (comboBox1.Text == "∧ - \"And\"")
+            {
+                Clipboard.SetText("∧");
+            }
+            else if (comboBox1.Text == "∨ - \"Or\"")
+            {
+                Clipboard.SetText("∨");
+            }
+            else if (comboBox1.Text == "¬ - \"Not\"")
+            {
+                Clipboard.SetText("¬");
+            }
+            else if (comboBox1.Text == "≡ - \"Exactly Equal To\"")
+            {
+                Clipboard.SetText("≡");
+            }
+            else if (comboBox1.Text == "⇒ - \"Implies\"")
+            {
+                Clipboard.SetText("⇒");
+            }
+            else if (comboBox1.Text == "⇔ - \"If and only if\"")
+            {
+                Clipboard.SetText("⇔");
+            }
+            else if (comboBox1.Text == "↓ - \"NOR\"")
+            {
+                Clipboard.SetText("↓");
+            }
+            else if (comboBox1.Text == "↑ - \"NAND\"")
+            {
+                Clipboard.SetText("↑");
+            }
+            else if (comboBox1.Text == "∃ - \"Existential Quantifier\"")
+            {
+                Clipboard.SetText("∃");
+            }
+            else if (comboBox1.Text == "∀ - \"Universal Quantifier\"")
+            {
+                Clipboard.SetText("∀");
+            }
+            else if (comboBox1.Text == "Ø - \"Empty Set\"")
+            {
+                Clipboard.SetText("Ø");
+            }
+            else if (comboBox1.Text == "∈ - \"Set Membership\"")
+            {
+                Clipboard.SetText("∈");
+            }
+            else if (comboBox1.Text == "∉ - \"Set Membership Negation\"")
+            {
+                Clipboard.SetText("∉");
+            }
+            else if (comboBox1.Text == "π - \"Pi Lower\"")
+            {
+                Clipboard.SetText("π");
+            }
+            else if (comboBox1.Text == "⊆ - \"Subset\"")
+            {
+                Clipboard.SetText("⊆");
+            }
+            else if (comboBox1.Text == "⊂ - \"Proper Subset\"")
+            {
+                Clipboard.SetText("⊂");
+            }
+            else if (comboBox1.Text == "∪ - \"Union\"")
+            {
+                Clipboard.SetText("∪");
+            }
+            else if (comboBox1.Text == "∩ - \"Intersection\"")
+            {
+                Clipboard.SetText("∩");
+            }
+            else if (comboBox1.Text == "⨄ - \"Disjoint Union\"")
+            {
+                Clipboard.SetText("⨄");
+            }
+            else if (comboBox1.Text == "↔ - \"Binary Relation\"")
+            {
+                Clipboard.SetText("↔");
+            }
+            else if (comboBox1.Text == "→ - \"Function\"")
+            {
+                Clipboard.SetText("→");
+            }
+            else if (comboBox1.Text == "◦ - \"Relation\"")
+            {
+                Clipboard.SetText("◦");
+            }
+            else if (comboBox1.Text == "• - \"Matrix\"")
+            {
+                Clipboard.SetText("•");
+            }
+            else if (comboBox1.Text == "⌊ - \"Floor/Round Open\"")
+            {
+                Clipboard.SetText("⌊");
+            }
+            else if (comboBox1.Text == "⌋ - \"Floor Close\"")
+            {
+                Clipboard.SetText("⌋");
+            }
+            else if (comboBox1.Text == "⌈ - \"Ceiling Open\"")
+            {
+                Clipboard.SetText("⌈");
+            }
+            else if (comboBox1.Text == "⌉ - \"Ceiling/Round Close\"")
+            {
+                Clipboard.SetText("⌉");
+            }
+            else if (comboBox1.Text == "Ω - \"Omega Upper\"")
+            {
+                Clipboard.SetText("Ω");
+            }
+            else if (comboBox1.Text == "ω – \"Omega Lower\"")
+            {
+                Clipboard.SetText("ω");
+            }
+            else if (comboBox1.Text == "α – \"Alpha Lower\"")
+            {
+                Clipboard.SetText("α");
+            }
+            else if (comboBox1.Text == "β – \"Beta Lower\"")
+            {
+                Clipboard.SetText("β");
+            }
+            else if (comboBox1.Text == "δ – \"Delta Lower\"")
+            {
+                Clipboard.SetText("δ");
+            }
+            else if (comboBox1.Text == "θ – \"Theta Lower\"")
+            {
+                Clipboard.SetText("θ");
+            }
+            else if (comboBox1.Text == "λ – \"Lambda Lower\"")
+            {
+                Clipboard.SetText("λ");
+            }
+            else if (comboBox1.Text == "μ – \"Mu Lower\"")
+            {
+                Clipboard.SetText("μ");
+            }
+            else if (comboBox1.Text == "Σ – \"Sigma Upper\"")
+            {
+                Clipboard.SetText("Σ");
+            }
+            else if (comboBox1.Text == "σ – \"Sigma Lower\"")
+            {
+                Clipboard.SetText("σ");
+            }
+            else if (comboBox1.Text == "Δ - \"Delta Upper\"")
+            {
+                Clipboard.SetText("Δ");
+            }
+            else if (comboBox1.Text == "∞ - \"Infinity\"")
+            {
+                Clipboard.SetText("∞");
+            }
+            else if (comboBox1.Text == "- - \"Combining Overline / Bar\"")
+            {
+                Clipboard.SetText(" ̅ ");
+            }
+            else
+            {
+                Clipboard.SetText("SGUI Error");
+            }
+        }
+    }
+}
